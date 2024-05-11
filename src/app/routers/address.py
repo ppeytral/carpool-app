336592707189c3989906e56c91db068c32fd0d75
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-11 15:35:29 +0200

    feat(routers): create a router for address entity

---
+++ src/app/routers/address.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+from config.database import get_session
+from fastapi import APIRouter
+from models.address import Address
+from schemas.address import AddressOut
+
+address_router = APIRouter(prefix="/address")
+
+
+@address_router.get(path="/", summary="Get all", response_model=list[AddressOut])
+def get_all():
+    with get_session() as s:
+        stmt = sa.select(Address)
+        result = s.scalars(stmt).all()
+
+        return list(result)
+
+
+@address_router.get(path="/{id}", summary="Get one", response_model=AddressOut)
+def get_one(id: int):
+    with get_session() as s:
+        result = s.get(Address, id)
+        return result
