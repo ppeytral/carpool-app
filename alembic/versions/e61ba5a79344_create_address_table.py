913205a2461e03ba278e2d8c99d95fe80f0b70df
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-11 12:09:39 +0200

    build(alembic): create address table and seed it

---
+++ alembic/versions/e61ba5a79344_create_address_table.py
@@ -0,0 +1,50 @@
+"""create address table
+
+Revision ID: e61ba5a79344
+Revises: 
+Create Date: 2024-05-11 11:30:42.355404
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "e61ba5a79344"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    address_table = op.create_table(
+        "address",
+        sa.Column("id", sa.Integer(), primary_key=True, autoincrement=True),
+        sa.Column("street", sa.String()),
+        sa.Column("postal_code", sa.String()),
+        sa.Column("city", sa.String()),
+        schema="carpool",
+    )
+
+    op.bulk_insert(
+        address_table,
+        [
+            {
+                "street": "13 impasse du flourege",
+                "postal_code": "84000",
+                "city": "Avignon",
+            },
+            {
+                "street": "rue de la coquillade",
+                "postal_code": "13540",
+                "city": "Puyricard",
+            },
+        ],
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("address", schema="carpool")
