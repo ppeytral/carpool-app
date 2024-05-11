4e4badcea14dfb1ab6ea48ba440d7aa6b5f7a445
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-11 12:16:42 +0200

    build(alembic): create school table and seed it

---
+++ alembic/versions/dd1a1864e19b_create_school_table.py
@@ -0,0 +1,47 @@
+"""create school table
+
+Revision ID: dd1a1864e19b
+Revises: e61ba5a79344
+Create Date: 2024-05-11 12:10:10.226747
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "dd1a1864e19b"
+down_revision: Union[str, None] = "e61ba5a79344"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    school_table = op.create_table(
+        "school",
+        sa.Column("id", sa.Integer(), primary_key=True, autoincrement=True),
+        sa.Column("name", sa.String()),
+        sa.Column("address_id", sa.Integer()),
+        schema="carpool",
+    )
+
+    op.bulk_insert(
+        school_table,
+        [
+            {
+                "name": "Nextech Avignon",
+                "address_id": 1,
+            },
+            {
+                "name": "Nextech Pertuis",
+                "address_id": 2,
+            },
+        ],
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("school", schema="carpool")
