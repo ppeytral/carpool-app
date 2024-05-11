31cb5cc056428f370d339a3e19e9aee5c2b226fe
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-11 15:36:56 +0200

    feat: update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,20 @@
+import uvicorn
 from fastapi import FastAPI
+from routers.address import address_router
 
 app = FastAPI()
+app.include_router(address_router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+    )
