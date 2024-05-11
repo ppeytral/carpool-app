0d80325170bc1ded49d07dd1a1923fc21ebe26dc
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-11 17:47:07 +0200

    build(env): update env config

--- src/app/config/config.py
+++ src/app/config/config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     test_db_username: str
     test_db_password: str
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    jwt_access_token_expire_minutes: int = 30
 
 
 def get_setting(name: str):
