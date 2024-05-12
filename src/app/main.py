801b95103a0261c469334bee6ab1413c81ee7ac2
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-12 10:20:49 +0200

    feat: add cors middleware

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from routers.address import address_router
 from routers.auth import auth_router
@@ -9,6 +10,20 @@ from routers.auth import auth_router
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:46585",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(address_router)
 app.include_router(auth_router)
 
