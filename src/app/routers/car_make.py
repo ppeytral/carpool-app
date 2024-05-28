e5be0ec9eaba66ad389047e8034187ab7d7074f7
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-28 09:36:27 +0200

    feat: add a route to fetch all models for a car brand

--- src/app/routers/car_make.py
+++ src/app/routers/car_make.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 from config.database import get_session
 from fastapi import APIRouter
 from models.car_make import CarMake
+from models.car_model import CarModel
 
 car_make_router = APIRouter(
     prefix="/car_make",
@@ -16,6 +17,16 @@ car_make_router = APIRouter(
 def get_all():
     with get_session() as s:
         stmt = sa.select(CarMake)
+        result = s.scalars(stmt).all()
+
+        return list(result)
+
+
+@car_make_router.get(path="/{make_id}/models", summary="Get all models of a make")
+def get_models(make_id: int):
+
+    with get_session() as s:
+        stmt = sa.select(CarModel).where(CarModel.car_make_id == make_id)
         result = s.scalars(stmt).all()
 
         return list(result)
