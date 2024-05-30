b042c1a894f61be98930bc25c030f6938d295a14
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-30 18:01:02 +0200

    feat: add a route to update a user

--- src/app/routers/user.py
+++ src/app/routers/user.py
@@ -1,8 +1,8 @@
 import sqlalchemy as sa
 from config.database import get_session
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from models.user import User
-from schemas.user import UserOut
+from schemas.user import UserOut, UserUpdate
 
 user_router = APIRouter(
     prefix="/user",
@@ -49,3 +49,32 @@ def delete_user_by_id(user_id: int):
         s.commit()
 
     return {"msg": f"Deleted user: '{user_id}'"}
+
+
+@user_router.put(
+    "/{user_id}",
+    summary="Update a user",
+)
+def update_user_by_id(user_id: int, new_user: UserUpdate):
+    with get_session() as s:
+        user_to_update = s.scalars(
+            sa.select(User).where(User.id == user_id)
+        ).first()
+        if not user_to_update:
+            return HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"user_id not found: '{user_id}'",
+            )
+        stmt = (
+            sa.update(User)
+            .where(User.id == user_id)
+            .values(
+                password=new_user.password or user_to_update.password,
+                student_id=new_user.student_id or user_to_update.student_id,
+                is_active=new_user.is_active or user_to_update.is_active,
+                username=new_user.username or user_to_update.username,
+            )
+        )
+        s.execute(stmt)
+        s.commit()
+        return {"msg": f"Updated user: '{user_id}'"}
--- src/app/schemas/user.py
+++ src/app/schemas/user.py
@@ -1,5 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from schemas.student import StudentOut
+
+
+class UserUpdate(BaseModel):
+    student_id: int | None = None
+    password: str | None = None
+    is_active: bool | None = None
+    username: str | None = None
 
 
 class UserOut(BaseModel):
