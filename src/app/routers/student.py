149ac2651032244613e8ca46811778061408035b
Patrice PEYTRAL <p.peytral@gmail.com>
2024-05-31 10:57:21 +0200

    feat: add a route to update a student

--- src/app/routers/student.py
+++ src/app/routers/student.py
@@ -1,9 +1,10 @@
 import sqlalchemy as sa
 from config.database import get_session
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from models.student import Student
 from models.user import Student
 from schemas.car import CarOut
+from schemas.student import StudentUpdate
 
 student_router = APIRouter(
     prefix="/student",
@@ -38,3 +39,38 @@ def get_cars_by_student_id(student_id: int):
         for c in result.cars:
             print(c)
         return result.cars
+
+
+@student_router.put(
+    "/{student_id}",
+    summary="Update a student",
+)
+def update_student_by_id(student_id: int, new_student: StudentUpdate):
+    with get_session() as s:
+        student_to_update = s.scalars(
+            sa.select(Student).where(Student.id == student_id)
+        ).first()
+        if not student_to_update:
+            return HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"user_id not found: '{student_id}'",
+            )
+        stmt = (
+            sa.update(Student)
+            .where(Student.id == student_id)
+            .values(
+                firstname=new_student.firstname or student_to_update.firstname,
+                lastname=new_student.lastname or student_to_update.lastname,
+                school_id=new_student.school_id or student_to_update.school_id,
+                address_id=new_student.address_id
+                or student_to_update.address_id,
+                email=new_student.email or student_to_update.email,
+                driving_licence_nb=new_student.driving_licence_nb
+                or student_to_update.driving_licence_nb,
+                driving_licence_validity=new_student.driving_licence_validity
+                or student_to_update.driving_licence_validity,
+            )
+        )
+        s.execute(stmt)
+        s.commit()
+        return {"msg": f"Updated student: '{student_id}'"}
--- src/app/schemas/student.py
+++ src/app/schemas/student.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from pydantic import BaseModel
-
 from schemas.address import AddressOut
 from schemas.school import SchoolOut
 
@@ -16,3 +15,14 @@ class StudentOut(BaseModel):
     email: str
     driving_licence_nb: str
     driving_licence_validity: datetime
+
+
+class StudentUpdate(BaseModel):
+
+    firstname: str | None = None
+    lastname: str | None = None
+    school_id: int | None = None
+    address_id: int | None = None
+    email: str | None = None
+    driving_licence_nb: str | None = None
+    driving_licence_validity: datetime | None = None
